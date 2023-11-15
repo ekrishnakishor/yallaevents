3cc0d5d04cfc0a9239d5bdb758136931a95ffeb8
ekrishnakishor <ekrishnakishor@gmail.com>
2023-11-15 22:17:36 +0530

    coming-soon

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+
+
+function App() {
+  return (
+    <div>
+      <div class="container">
+       
+
+        <h1>Coming Soon</h1>
+        <p>This website is currently under construction and will be launched in the near future.</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <p>© 2023 Yalla Events</p>
+    </div>
+  )
+}
+
+export default Footer
